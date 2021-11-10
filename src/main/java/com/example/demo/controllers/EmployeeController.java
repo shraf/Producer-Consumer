7599a3fee887877b7e257032182a0e4775ff6349
sharaf eldeen <eldeenshraf@gmail.com>
2021-11-11 01:06:32 +0200

    fix cors

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/employee")
 public class EmployeeController {
